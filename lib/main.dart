9aa3fda76d2fbf06b73597e8051b4a39bf443b2c
jessyredington <72144404+jessyredington@users.noreply.github.com>
2020-10-09 14:12:59 -0400

    Add files via upload
    added bottom nav bar to  main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,19 @@ import 'screens/event_tags_page.dart';
 import 'screens/favorites_page.dart';
 import 'screens/map_page.dart';
 import 'screens/notification_page.dart';
-import 'models/event.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+
+
 
 
 
 void main() => runApp(Scrappy());
 
 class Scrappy extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,7 +29,7 @@ class Scrappy extends StatelessWidget {
         bottomAppBarColor: Colors.white,
         accentColor: Colors.amberAccent
       ),
-      home: EventsBankPage(),
+      home: BottomNav(),
       routes: {
         '/event_bank':(context)=>EventsBankPage(),
         '/event_details': (context)=>EventDetailsPage(),
@@ -35,5 +41,59 @@ class Scrappy extends StatelessWidget {
     );
   }
 }
+
+class BottomNav extends StatefulWidget {
+  @override
+  _BottomNavState createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  @override
+  Widget build(BuildContext context) {
+    int _currentIndex=0;
+    final List<Widget> _children=<Widget>[
+      EventsBankPage(),
+      FavoritesPage(),
+      CalendarPage(),
+      NotificationPage(),
+      MapPage(),
+    ];
+
+    void onTappedBar(int index)
+    {setState(() {
+      _currentIndex=index;
+    });}
+    return new Scaffold(
+      body: _children.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+
+        onTap: onTappedBar,
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+
+              icon: new Icon(FontAwesomeIcons.home),
+              title: new Text("Home")),
+          BottomNavigationBarItem(
+              icon: new Icon(FontAwesomeIcons.star),
+              title: new Text("Favorites")),
+          BottomNavigationBarItem(
+              icon: new Icon(FontAwesomeIcons.calendar),
+              title: new Text("Calendar")),
+          BottomNavigationBarItem(
+              icon: new Icon(FontAwesomeIcons.bell),
+              title: new Text("Notifications")),
+          BottomNavigationBarItem(
+              icon: new Icon(FontAwesomeIcons.compass),
+              title:  new Text("Campus Maps")),
+        ],
+      ),
+
+    );
+  }
+}
+
+
 
 
