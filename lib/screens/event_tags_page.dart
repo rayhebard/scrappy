c5f39bfc6aa82f4c3023504cfa2af9342fba9df5
jessyredington <72144404+jessyredington@users.noreply.github.com>
2020-10-13 11:58:18 -0400

    updated tags page UI
    updated tags page UI

--- lib/screens/event_tags_page.dart
+++ lib/screens/event_tags_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:scrappy/constants.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class EventTagsPage extends StatefulWidget {
   @override
@@ -8,19 +9,98 @@ class EventTagsPage extends StatefulWidget {
 }
 
 class _EventTagsPageState extends State<EventTagsPage> {
+  bool _isChecked = true;
+  void onChanged(bool value){
+    setState(() {
+_isChecked=value;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(
-          title: Text('Scrappy')
+          title: Text('Event Tags')
       ),
-      body: SafeArea(
+      body:
+      new Container(
+
           child: Column(
-            children: <Widget>[
-              Text('Event Tags', style: kTItleTextStyle,)
+           
+            children:<Widget>[
+
+              Padding(
+              padding: EdgeInsets.only(top: 10.0)),
+
+              Expanded(
+
+                  child: ListView(
+                  children: <Widget>[
+
+                    new Text("Time of Day Filters", style: kLabelTextStyle2,),
+                    new CheckboxListTile(
+                        title: new Text("Morning Events", style: kLabelTextStyle ),
+                        secondary: const Icon(FontAwesomeIcons.checkCircle),
+                        activeColor: Colors.amberAccent,
+
+                        value: _isChecked,
+                        onChanged: (bool value){onChanged(value);}),
+                    new CheckboxListTile(
+                        title: new Text("Afternoon Events", style: kLabelTextStyle),
+                        activeColor: Colors.amberAccent,
+                        secondary: const Icon(FontAwesomeIcons.checkCircle),
+                        value: _isChecked,
+                        onChanged: (bool value){onChanged(value);}),
+                    new CheckboxListTile(
+                        title: new Text("Evening Events", style: kLabelTextStyle),
+                        activeColor: Colors.amberAccent,
+                        secondary: const Icon(FontAwesomeIcons.checkCircle),
+                        value: _isChecked,
+                        onChanged: (bool value){onChanged(value);}),
+                    new Text("Event Meeting Type", style: kLabelTextStyle2,),
+                    new CheckboxListTile(
+                        title: new Text("In-Person Events", style: kLabelTextStyle),
+                        activeColor: Colors.amberAccent,
+                        secondary: const Icon(FontAwesomeIcons.checkCircle),
+                        value: _isChecked,
+                        onChanged: (bool value){onChanged(value);}),
+                    new CheckboxListTile(
+                        title: new Text("Virtual Events", style: kLabelTextStyle),
+                        activeColor: Colors.amberAccent,
+                        secondary: const Icon(FontAwesomeIcons.checkCircle),
+                        value: _isChecked,
+                        onChanged: (bool value){onChanged(value);}),
+                    new Text("Event Type", style: kLabelTextStyle2,),
+                    new CheckboxListTile(
+                        title: new Text("Lecture Events", style: kLabelTextStyle),
+                        activeColor: Colors.amberAccent,
+                        secondary: const Icon(FontAwesomeIcons.checkCircle),
+                        value: _isChecked,
+                        onChanged: (bool value){onChanged(value);}),
+                    new CheckboxListTile(
+                        title: new Text("Career Networking Events", style: kLabelTextStyle),
+                        activeColor: Colors.amberAccent,
+                        secondary: const Icon(FontAwesomeIcons.checkCircle),
+                        value: _isChecked,
+                        onChanged: (bool value){onChanged(value);}),
+                    new CheckboxListTile(
+                        title: new Text("Social Events", style: kLabelTextStyle),
+                        activeColor: Colors.amberAccent,
+                        secondary: const Icon(FontAwesomeIcons.checkCircle),
+                        value: _isChecked,
+                        onChanged: (bool value){onChanged(value);}),
+                    new CheckboxListTile(
+                        title: new Text("Competition Events", style: kLabelTextStyle),
+                        activeColor: Colors.amberAccent,
+                        secondary: const Icon(FontAwesomeIcons.checkCircle),
+                        value: _isChecked,
+                        onChanged: (bool value){onChanged(value);}),
+  ]
+              )),
+
+
             ],
-          )
-      ),
+          ),
+  )
     );
   }
 }
