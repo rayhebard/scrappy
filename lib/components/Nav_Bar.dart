4ed33c1bc6754a8810ef86a67cd684feae23b277
jessyredington <72144404+jessyredington@users.noreply.github.com>
2020-10-15 11:25:50 -0400

    navnar stacking fix

---
+++ lib/components/Nav_Bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:scrappy/screens/favorites_page.dart';
+import '../screens/event_tags_page.dart';
+import '../screens/calendar_page.dart';
+import '../screens/notification_page.dart';
+import '../screens/map_page.dart';
+import '../screens/events_bank_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Navbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+
+          Expanded(child:FlatButton(
+    onPressed: () => Navigator.pushReplacementNamed(context, EventsBankPage.id),
+    child: Icon(FontAwesomeIcons.home, color: Colors.white)),
+          ),
+          Expanded(child: FlatButton(
+    onPressed: () => Navigator.pushReplacementNamed(context, FavoritesPage.id),
+    child: Icon(FontAwesomeIcons.star, color: Colors.white)),
+          ),
+          Expanded(child:FlatButton(
+    onPressed: () => Navigator.pushReplacementNamed(context, NotificationPage.id),
+    child: Icon(FontAwesomeIcons.bell, color: Colors.white)), 
+          ),
+          Expanded(child: FlatButton(
+              onPressed: () => Navigator.pushReplacementNamed(context, MapPage.id),
+              child: Icon(FontAwesomeIcons.compass, color: Colors.white)),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
