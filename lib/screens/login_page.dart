beaba80c8bc31a3a0e7abe5f41cd0cba6cb7a045
Random Hebard <rayhebard@gmail.com>
2020-10-23 07:48:42 -0400

    Login Page Redesign Prototype

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -15,106 +15,139 @@ class _LoginPageState extends State<LoginPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       backgroundColor: Colors.amberAccent.shade100,
         appBar: AppBar(
           title: Text('Scrappy Log-In Page'),
         ),
         body:
-        Padding(
-            padding: EdgeInsets.all(10),
-            child: ListView(
-              children: <Widget>[
-               Center(
-                 child: new Image.asset(
-                   'images/owl_wink.jpg',
-                   fit: BoxFit.fill,
-                 ),
-               ),
-
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'User Name',
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors.grey, width: 3.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors.amber, width: 3.0),
+        Container(
+          // decoration: BoxDecoration(image: DecorationImage(
+          //   image: AssetImage("images/owl_wink.jpg"), fit: BoxFit.fill)
+          // ),
+          child: Padding(
+              padding: EdgeInsets.all(10),
+              child: ListView(
+                children: <Widget>[
+                 // Center(
+                 //   child: new Image.asset(
+                 //     'images/owl_wink.jpg',
+                 //     fit: BoxFit.fill,
+                 //   ),
+                 // ),
+                  Center(
+                    child: Text(
+                      'Scrappy',
+                      style: TextStyle(
+                          fontFamily: 'Pacifico',
+                          fontSize: 40,
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold ),
+                    ),
+                  ),
+                  SizedBox(height: 20,
+                    width: 150.0,
+                    child: Divider(
+                      color: Colors.teal.shade100,
+                    ),
+                  ),
+                  CircleAvatar(
+                    radius: 120.0,
+                    backgroundImage: AssetImage('images/owl_wink.jpg') ,
+                  ),
+                  SizedBox(height: 20,
+                    width: 150.0,
+                    child: Divider(
+                      color: Colors.teal.shade100,
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      controller: nameController,
+                      style: TextStyle(color: kCardColor),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'User Name',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.grey, width: 3.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.black, width: 3.0),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors.grey, width: 3.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors.amber, width: 3.0),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: TextField(
+                      obscureText: true,
+                      style: TextStyle(color: kCardColor),
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.grey, width: 3.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.black, width: 3.0),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Container(
+                  Container(
 
-                    height: 70,
-                    padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.amber,
-                      child: Text(
-                          'Login',
-                          style: TextStyle(fontSize: 20),
-                      ),
+                      height: 70,
+                      padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                      child: RaisedButton(
+                        textColor: Colors.amberAccent.shade100,
+                        color: kCardColor,
+                        child: Text(
+                            'Login',
+                            style: TextStyle(fontSize: 20),
+                        ),
 
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context){
-                              return LoadingScreen();
-                            }
-                            )
-                        );
-                        print(nameController.text);
-                        print(passwordController.text);
-                      },
-                    )),
-                Container(
-                    height: 70,
-                    padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-                    child:
-                        FlatButton(
-                          textColor: Colors.amber,
-                          color: Colors.grey,
-                          child: Text(
-                            'Continue as Guest ->',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context){
-                                  return LoadingScreen();
-                                }
-                                )
-                            );
-                          },
-                        )
-                    )
-              ],
-            )
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context){
+                                return LoadingScreen();
+                              }
+                              )
+                          );
+                          print(nameController.text);
+                          print(passwordController.text);
+                        },
+                      )),
+                  Container(
+                      height: 70,
+                      padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                      child:
+                          FlatButton(
+                            textColor: Colors.amber,
+                            color: Colors.grey,
+                            child: Text(
+                              'Continue as Guest ->',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context){
+                                    return LoadingScreen();
+                                  }
+                                  )
+                              );
+                            },
+                          )
+                      )
+                ],
+              )
+          ),
         )
     );
   }
