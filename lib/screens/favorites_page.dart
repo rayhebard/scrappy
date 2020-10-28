2839da76650c96da2fd6cf74c4e7f0cbee6d1657
jessyredington <72144404+jessyredington@users.noreply.github.com>
2020-10-28 19:22:54 -0400

    added favorite events are now veiwable in the page

--- lib/screens/favorites_page.dart
+++ lib/screens/favorites_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:scrappy/components/nav_bar.dart';
+import 'package:scrappy/models/Favorites.dart';
+import 'package:scrappy/services/database_helper.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:scrappy/constants.dart';
 class FavoritesPage extends StatefulWidget {
   static const String id = '/favorites';
 
@@ -9,13 +13,42 @@ class FavoritesPage extends StatefulWidget {
 }
 
 class _FavoritesPageState extends State<FavoritesPage> {
+  final dbHelper = DatabaseHelper.instance;
+  List<Favorites> favList = new List();
 
+  @override
+  void initState() {
+    super.initState();
+
+    DatabaseHelper.instance.queryAllRows().then((value) {
+      setState(() {
+        value.forEach((element) {
+          favList.add(Favorites(id: element['id'], title: element["title"]));
+        });
+      });
+    }).catchError((error) {
+      print(error);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: Navbar(),
-      appBar:AppBar(
+          appBar:AppBar(
           title: Text('Scrappy')
+      ),
+      body: Container(
+
+        child: ListView.builder(itemBuilder: (ctx, index) {
+          if (index == favList.length) return null;
+          return ListTile(
+
+            tileColor: Colors.amber,
+            title: Text(favList[index].title, textAlign: TextAlign.center, ),
+            leading: Icon(FontAwesomeIcons.star,),
+            trailing: Icon(FontAwesomeIcons.trashAlt),
+          );
+        }),
       ),
     );
   }
