2f4e16721c63fb9545425f24837e6964f546d2b1
Random Hebard <rayhebard@gmail.com>
2020-11-18 20:52:32 -0500

    Create Tab Controller

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -12,50 +12,84 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  PDFDocument kennesaw;
-  PDFDocument marietta;
-  var _isLoading = true;
+    var document;
+    var _isLoading= true;
 
-  loadMaps() async{
-    kennesaw= await PDFDocument.fromAsset("images/kennesaw-parking-map-fall-2020.pdf");
-    marietta= await PDFDocument.fromAsset("images/marietta-map-fall-2020-v5.pdf");
+  loadKennesaw() async{
+     document = await PDFDocument.fromAsset("images/kennesaw-parking-map-fall-2020.pdf");
+     print("Load Kennesaw");
+     setState(() {
+       _isLoading = false;
+     });
+  }
+
+  getKennesaw(){
+    loadKennesaw();
+  }
+
+  loadMarrietta() async{
+    document = await PDFDocument.fromAsset("images/marietta-map-fall-2020-v5.pdf");
+    print("Load Marrietta");
     setState(() {
-      kennesaw = kennesaw;
-      marietta = marietta;
       _isLoading = false;
     });
   }
 
-  @override
+  getMarrietta(){
+    loadMarrietta();
+  }
+
+
+   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    loadMaps();
+    loadKennesaw();
   }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
-      child: Scaffold(
-        bottomNavigationBar: Navbar(),
-        appBar: AppBar(
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'Kennesaw'),
-              Tab(text:'Marietta'),
-            ],
-          ),
-          title: Text('Campus Maps'),
-        ),
-        body: TabBarView(
-          children: [
-            _isLoading? Center(child: CircularProgressIndicator())
-            : PDFViewer(document: kennesaw),
-            _isLoading? Center(child: CircularProgressIndicator())
-                : PDFViewer(document: marietta),
-          ],
-        ),
+      // The Builder widget is used to have a different BuildContext to access
+      // closest DefaultTabController.
+      child: Builder(
+          builder: (BuildContext context) {
+            final TabController tabController = DefaultTabController.of(context);
+            tabController.addListener(() {
+              if (!tabController.indexIsChanging) {
+
+                setState(() {
+                  _isLoading = true;
+                });
+
+                if(tabController.index == 0 && _isLoading == false){
+                  getKennesaw();
+                }
+                if(tabController.index == 1 && _isLoading == false){
+                  getMarrietta();
+                }
+                // Your code goes here.
+                // To get index of current tab use tabController.index
+              }
+            });
+            return Scaffold(
+              appBar: AppBar(
+                bottom: TabBar(
+                  tabs: [
+                    Tab(text: "Kennesaw"),
+                    Tab(text:"Marrietta")
+                  ]
+                ),
+              ),
+              body: TabBarView(
+                children: [
+                  _isLoading? CircularProgressIndicator(): PDFViewer(document: document),
+                  _isLoading? CircularProgressIndicator(): PDFViewer(document: document)
+                ]
+              ),
+            );
+          }
       ),
     );
   }
